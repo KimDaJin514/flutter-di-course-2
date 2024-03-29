f2cdf1ae54b4e604338a0a40b3dbf4b8daedc2dc
KimDaJin <kimdajin514@gmail.com>
2024-03-29 14:15:26 +0900

    feat: Add main view model

---
+++ lib/presentation/main_state.dart
@@ -0,0 +1,18 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../domain/model/store.dart';
+
+part 'main_state.freezed.dart';
+
+part 'main_state.g.dart';
+
+@freezed
+class MainState with _$MainState {
+  const factory MainState({
+    @Default([]) List<Store> stores,
+    @Default(false) bool isLoading,
+  }) = _MainState;
+
+  factory MainState.fromJson(Map<String, dynamic> json) => _$MainStateFromJson(json);
+}
---
+++ lib/presentation/main_viewmodel.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:mask_info_app/domain/use_case/get_near_by_stores_use_case.dart';
+import 'package:mask_info_app/presentation/main_state.dart';
+
+class MainViewModel with ChangeNotifier {
+  final GetNearByStoresUseCase _getNearByStoresUseCase;
+
+  MainState _state = const MainState();
+  MainState get state => _state;
+
+  MainViewModel(this._getNearByStoresUseCase);
+
+  Future<void> getStores() async {
+    _state = state.copyWith(isLoading: true);
+    notifyListeners();
+
+    _state = state.copyWith(
+      stores: await _getNearByStoresUseCase.execute(),
+      isLoading: false
+    );
+
+    notifyListeners();
+  }
+}
