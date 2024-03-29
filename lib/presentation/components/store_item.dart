8753c92f3ca3e331a49c56ffbf3a3da1764299b7
KimDaJin <kimdajin514@gmail.com>
2024-03-29 14:23:37 +0900

    feat: Add ui setting before vm di

---
+++ lib/presentation/components/store_item.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../domain/model/store.dart';
+
+class StoreItem extends StatelessWidget {
+  final Store store;
+  const StoreItem({super.key, required this.store});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Text(store.name),
+            Text(store.address),
+            Text('${store.distance / 1000.0}km'),
+          ],
+        ),
+        Column(
+          children: [
+            Text(store.remainStatus),
+            Text('100개 이상'),
+          ],
+        )
+      ],
+    );
+  }
+}
